da4525f0c224d6aed27eec4aef0d63a1018b801d
Muhammed Rajab <muhammedrajab101@gmail.com>
2025-10-18 01:39:29 +0530

    added a basic ray class to keep track of rays

---
+++ src/ray.cpp
@@ -0,0 +1,8 @@
+#include "ray.hpp"
+
+// constructor
+Ray::Ray(const Point3 &origin, const Vec3 &direction)
+    : orig(origin), dir(direction) {}
+
+// linearly interpolates the ray and give a point at 't'
+Point3 Ray::at(double t) const { return orig + dir * t; }
