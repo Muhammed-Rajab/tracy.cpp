f9e1ec490c55a84c9abb2ba33e91d115ab8ab6eb
Muhammed Rajab <muhammedrajab101@gmail.com>
2025-10-18 17:35:37 +0530

    started working on sphere hittable. now only accepts radius >= 0

--- src/sphere.cpp
+++ src/sphere.cpp
@@ -1,6 +1,6 @@
 #include "sphere.hpp"
 
 Sphere::Sphere(const Point3 &center, const double radius)
-    : center(center), radius(radius) {}
+    : center(center), radius(std::fmax(0, radius)) {}
 
 bool Sphere::hit(const Ray &r, Interval ray_t, HitRecord &rec) const {}
