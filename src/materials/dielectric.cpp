74b8ab9a7ff4f606fbf7c9829a27ac2a88795710
Muhammed Rajab <muhammedrajab101@gmail.com>
2025-10-20 18:27:29 +0530

    shit works.

--- src/materials/dielectric.cpp
+++ src/materials/dielectric.cpp
@@ -11,10 +11,25 @@ bool Dielectric::scatter(const Ray &r_in, const HitRecord &rec,
   double ri = rec.front_face ? (1.0 / refraction_index) : refraction_index;
 
   Vec3 unit_direction = unit_vector(r_in.direction());
-  Vec3 refracted = refract(unit_direction, rec.normal, ri);
+  double cos_theta = std::fmin(dot(-unit_direction, rec.normal), 1.0);
+  double sin_theta = std::sqrt(1.0 - cos_theta * cos_theta);
 
-  scattered = Ray(rec.p, refracted);
+  bool cannot_refract = ri * sin_theta > 1.0;
+  Vec3 direction;
+
+  if (cannot_refract || reflectance(cos_theta, ri) > random_double()) {
+    direction = reflect(unit_direction, rec.normal);
+  } else {
+    direction = refract(unit_direction, rec.normal, ri);
+  }
+
+  scattered = Ray(rec.p, direction);
   return true;
 }
 
-double Dielectric::reflectance(double cosine, double refraction_index) {}
+double Dielectric::reflectance(double cosine, double refraction_index) {
+  auto r0 = (1 - refraction_index) / (1 + refraction_index);
+  r0 = r0 * r0;
+
+  return r0 + (1 - r0) * std::pow((1 - cosine), 5);
+}
